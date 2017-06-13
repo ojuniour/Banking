eea8f99eb97259e4607f692768154bc2d3ecd8e9
denni <denniss4000@hotmail.com>
2017-06-13 15:51:06 -0400

    v3

---
+++ Bank/website/Assets/Scripts/appController.js
@@ -0,0 +1,24 @@
+﻿var App = angular.module("App", ['ui.router']);
+
+
+App.controller("PageTitle", pageTitle);
+
+function pageTitle($scope) {
+    $scope.title = "Welcome Page";
+};
+
+
+App.directive('lazy', function () {
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function (scope, elem, attr) {
+            if (attr.type == 'js') {
+                var code = elem.text();
+                var f = new Function(code);
+                f();
+                element.append(f());
+            }
+        }
+    };
+});
---
+++ Bank/website/Assets/Scripts/routes.js
@@ -0,0 +1,25 @@
+﻿App.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    var loginPage = {
+        name: 'login',
+        url: '/login',
+        templateUrl: 'Component/login/login.html'
+    };
+
+    var dashboardPage = {
+        name: 'dashboard',
+        url: '/dashboard',
+        templateUrl: 'Component/dashboard/dashboard.html'
+    };
+
+    var logout = {
+        name: 'logout',
+        url: '/logout',
+        templateUrl: 'Component/logout/logout.html'
+    }
+
+    $stateProvider.state(loginPage);
+    $stateProvider.state(dashboardPage);
+    $locationProvider.html5Mode(true);
+}); 
