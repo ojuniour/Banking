09e535d00caef9626ae9a5532db42f362726152d
denni <denniss4000@hotmail.com>
2017-06-27 12:04:11 -0400

    initial

---
+++ magazineShop/magazineShop/Script/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('magazine', []);
+
+
+app.controller('customers', customerFN);
+
+function customerFN($scope){
+    $scope.custList = [
+        'adekunle owolabi',
+        'king kuta',
+        'Indian wife'
+    ];
+    $scope.cust = null;
+
+    var custSubmit = function () {
+        $scope.custList.append($scope.cust);
+    }
+    //$scope.custSub = 
+
+}
