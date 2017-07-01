8db2bfd696ac58bd6fdb220431710c4b0ccd7fb0
denni <denniss4000@hotmail.com>
2017-07-01 17:23:57 -0400

    Revert "Revert "api""
    This reverts commit f9e6606169b55fad1c68a33a8283b9271a82fc5e.

--- magazineShop/magazineShop/Script/app.js
+++ magazineShop/magazineShop/Script/app.js
@@ -3,17 +3,79 @@
 
 app.controller('customers', customerFN);
 
-function customerFN($scope){
-    $scope.custList = [
-        'adekunle owolabi',
-        'king kuta',
-        'Indian wife'
-    ];
-    $scope.cust = null;
+function customerFN($scope) {
 
-    var custSubmit = function () {
-        $scope.custList.append($scope.cust);
+    $scope.fame = [{
+        name: 'Ade',
+        id: "500"
+    }]
+
+    $scope.res = [3, 4, 5];
+
+
+    $scope.custList = [{
+        name: 'adekunle owolabi',
+        sub: [{
+            magazine: 'whatever',
+            startDate: 'Today',
+            endDate: 'Tomoroow',
+            rate: '35.99'
+        },
+        {magazine: 'Innow',
+        startDate: 'Today',
+        endDate: 'Tomoroow',
+        rate: '35.99'}]
+    }];
+
+    $scope.magazine = [{
+        name: 'Time Magazine',
+        rate: '$9.99'
+    },
+    {
+        name: 'The New Yorker',
+        rate: '$34.99'
+    }];
+
+
+    $scope.custSubmit = function () {
+        var custObj = {
+            name: $scope.custName,
+            sub: [{
+                magazine: $scope.custMagazine == null ? 'No Subscription' : $scope.custMagazine,
+                startDate: $scope.custStartDate == null ? 'No Start Date' : $scope.custStartDate,
+                endDate: $scope.custEndDate == null ? 'No End Date' : $scope.custEndDate,
+                rate: $scope.custRate == null ? 'No Rate' : $scope.custRate
+            }]
+        }
+        $scope.custName == null ? false : $scope.custList.push(custObj);
+        $scope.custName = null;
     }
-    //$scope.custSub = 
 
+    $scope.custSelected = function () {
+        console.log($scope.customers.sub);
+       // $scope.sub.push()
+
+    }
+
+
+
+
+    $scope.subscribe = function () {
+        var shortDate = function (date) {
+            return date.getMonth()+'/'+date.getDate()+'/'+date.getFullYear();
+        }
+        $scope.newSub = {
+            magazine: $scope.mag.name,
+            startDate: shortDate($scope.startDate),
+            endDate: shortDate($scope.endDate),
+            rate: $scope.mag.rate
+        }
+        $scope.customers.sub.push($scope.newSub);
+        console.log($scope.startDate.get)
+    }
+
+    $scope.unsubscribe = function (custSub) {
+        var index = $scope.customers.sub.indexOf(custSub);
+        $scope.customers.sub.splice(index,1)
+    }
 }
