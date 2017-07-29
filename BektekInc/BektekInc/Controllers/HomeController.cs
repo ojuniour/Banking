f425d4836d20938cb55f7abac6b48cfcbb87c867
denni <denniss4000@hotmail.com>
2017-07-29 17:00:35 -0400

    testoff

--- BektekInc/BektekInc/Controllers/HomeController.cs
+++ BektekInc/BektekInc/Controllers/HomeController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Routing;
+using System.Web.Security;
+using BektekInc.Models;
 namespace BektekInc.Controllers
 {
-    [Authorize]
+    [Authorization]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
         }
-        [AllowAnonymous]
+        
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -27,5 +29,40 @@ namespace BektekInc.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string registerUser()
+        {
+            LoginModels user;
+            userStorage userstorage = new userStorage(new database());
+            userManager usermananager = new userManager(userstorage);
+            user = new LoginModels() { //using Object Initializer
+                Email = "fuck yoou",
+                UserName = "atme"
+            };
+            var result = usermananager.CreateAsync(user); //add user to manager
+            return result.IsCompleted ? "User Added" : result.IsFaulted.ToString();
+
+        }
     }
+
+
+    public class AuthorizationAttribute : FilterAttribute, IAuthorizationFilter
+    {
+         void IAuthorizationFilter.OnAuthorization(AuthorizationContext filterContext)
+        {
+            filterContext.HttpContext.User.ToString();
+            filterContext.HttpContext.ToString();
+            filterContext.Controller.ViewBag.OnAuthorization = "IAuthorizationFilter.OnAuthorization filter called";
+            //ApplicationSignInManager.Create()
+            filterContext.Result = new RedirectToRouteResult(
+                 new RouteValueDictionary(
+                     new { action = "login", Controller = "Account" }));
+        }
+
+        public string User { get; set; }
+        public string Access { get; set; }
+    }
+
 }
---
+++ BektekInc/BektekInc/Models/LoginModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace BektekInc.Models
+{
+    public class LoginModels : IdentityUser
+    {
+       public string email { get; set; }
+    }
+
+    //database
+    public class database : IdentityDbContext<LoginModels>
+    {
+        public database() : base("connectionString")
+        {
+
+        }
+
+    }
+
+    //Defines method for CRUD
+    public class userStorage : UserStore<LoginModels>
+    {
+        public userStorage(database db): base(db)
+        {
+
+        }
+
+    }
+
+    //Manage Users (CRUD) ues userstorage to connect to the database
+    public class userManager : UserManager<LoginModels>
+    {
+        public userManager(IUserStore<LoginModels> db) : base(db)
+        {
+
+        }
+    }
+
+
+}
